d8226e5d12939cfe6a99851e0d11c60aefd3a404
Peter Downs <peterldowns@gmail.com>
2016-05-26 22:10:50 +0600

    Major clean up.

--- bookshrink.py
+++ bookshrink.py
@@ -2,6 +2,110 @@
 import re
 from collections import Counter
 
+class Text(object):
+  def __init__(self, input_text):
+    if not isinstance(input_text, unicode):
+      input_text = input_text.decode('utf8')
+
+    # Step 1: split the input into sentences.
+    sentences = to_sentences(input_text.strip())
+
+    # Count each unique word's frequency in the input text.
+    word_weights = Counter()
+    for word in to_words(input_text):
+      # Don't count frequencies for stop words.
+      if is_stopword(word):
+        continue
+      word_weights[word] += 1
+
+    # The number of times a word has appeared in the text is its raw
+    # weight. Modify these weights by certain conditions not based on
+    # appearance count.
+    for word in word_weights:
+      # Modify proper noun weights (but not ALL_CAPS words)
+      if word.istitle() and not word.isupper():
+        word_weights[word] *= 1.2
+
+    # Assign each individual sentence a weight/score based on the weights
+    # of the words of which they are comprised.
+    lengths = []
+    average_length = 0.0
+    scores = {}
+    for i, raw_sentence in enumerate(sentences):
+      s_clean = clean(raw_sentence)
+      s_words = s_clean.split()
+      s_length = len(s_words)
+
+      # Update the average length of all sentences based on the length of this
+      # sentence.
+      average_length = float(average_length * i + s_length) / (i + 1)
+
+      # Sum the weights of the words in this sentence. This is the basic
+      # concept of a sentence's score.
+      s_sum = 0.0
+      for s_word in s_words:
+        s_sum += word_weights[s_word]
+
+      # Sentence scores are normalized by length; if the sentence has no words
+      # (possible because of our very naive sentence splitting trick), give it
+      # a value of 0.
+      scores[raw_sentence] = float(s_sum)/s_length if s_length else 0.0
+
+    # Despite earlier normalization, the final sentence scores do depend on
+    # length.
+    for raw_sentence in sentences:
+      s_length = len(clean(raw_sentence).split())
+      if s_length < (average_length / 1.5): # punish short sentences
+        scores[raw_sentence] /= 3
+      elif s_length > (average_length * 1.2): # reward longer sentences
+        scores[raw_sentence] *= 3
+
+    self.sentences = sentences # ["sentence", ...]
+    self.scores = scores # {"sentence": 3.52, ...}
+    self.word_weights = word_weights # {"word": 5.1}
+    self.sorted_sentences = sorted(
+            scores.keys(),
+            key=lambda k: scores[k],
+            reverse=True) # ["most important sentence", ..., "least important"]
+    self.highest_sentence_score = scores[self.sorted_sentences[0]] # 9.1
+    self.average_sentence_length = average_length # 5.18
+    self.sorted_words = sorted(
+            word_weights.keys(),
+            key=lambda key: word_weights[key],
+            reverse=True) # ["most important word", ..., "least important"]
+    self.highest_word_score = word_weights[self.sorted_words[0]] # 51.6
+
+  def get_results(self):
+    # Generates tuples (absolute_score, relative_score, sentence) in descending
+    # score order.
+    for sentence in self.sorted_sentences:
+      score = self.scores[sentence]
+      yield (score, score/self.highest_sentence_score, sentence)
+
+
+def is_stopword(word):
+  return word.lower() in _stopwords
+
+
+def clean(text):
+  """ Get rid of any non-alphabetic character and clean up whitespace. """
+   # Remove everything that isn't an alphabet character (only interested
+   # in words, not numbers or punctuation).
+  text = re.sub(r'[^a-zA-Z]', ' ', text)
+  # Collapse whitespace in any amount or type (tabs, newlines, etc.) into
+  # a single space.
+  text = re.sub(r'\s+', ' ', text)
+  return text.strip()
+
+
+def to_sentences(text):
+  return _sentence_splitter.split(text)
+
+
+def to_words(text):
+  return clean(text).split()
+
+
 # The ~100 most common words in the english language (which will be ignored in
 # the analysis), taken from
 # http://www.duboislc.org/EducationWatch/First100Words.html
@@ -13,8 +117,6 @@ time has look two more write go see number no way could people my than first
 water been call who oil its now find long down day did get come made may part
 am let""".split())
 
-def is_stopword(word):
-  return word.lower() in _stopwords
 
 # A simple regex for splitting text into sentences, ignoring some common
 # abbreviations, numbers, and other phrases that also use a '.' to mean
@@ -39,102 +141,3 @@ _sentence_splitter = re.compile(
        )
        [\s]+?
        (?=[^a-z0-9])''', re.VERBOSE)
-
-def clean(text):
-  """ Get rid of any non-alphabetic character and clean up whitespace. """
-   # Remove everything that isn't an alphabet character (only interested
-   # in words, not numbers or punctuation).
-  text = re.sub(r'[^a-zA-Z]', ' ', text)
-  # Collapse whitespace in any amount or type (tabs, newlines, etc.) into
-  # a single space.
-  text = re.sub(r'\s+', ' ', text)
-  return text.strip()
-
-def to_sentences(text):
-  return _sentence_splitter.split(text)
-
-def to_words(text):
-  return clean(text).split()
-
-
-class Text(object):
-  def get_results(self):
-    # Returns a list of tuples (absolute_score, relative_score, sentence) in
-    # descending score order.
-    for sentence in self.sorted_sentences:
-      score = self.scores[sentence]
-      yield (score, score/self.highest_s_score, sentence)
-
-  def __init__(self, input_text):
-    self.inputstr = input_text
-    if not isinstance(self.inputstr, unicode):
-        self.inputstr = self.inputstr.decode('utf8')
-
-    # Step 1: split the input into sentences.
-    self.sentences = to_sentences(self.inputstr.strip())
-
-    # Count each unique word's frequency in the input text.
-    self.words = to_words(self.inputstr)
-    self.word_weights = Counter()
-    for word in self.words:
-        # Don't count frequencies for stop words.
-        if is_stopword(word):
-            continue
-        self.word_weights[word] += 1
-
-    # The number of times a word has appeared in the text is its raw
-    # weight. Modify these weights by certain conditions not based on
-    # appearance count.
-    for word in self.word_weights:
-        # Modify proper noun weights (but not ALL_CAPS words)
-        if word.istitle() and not word.isupper():
-            self.word_weights[word] *= 1.2
-
-    # Assign each individual sentence a weight/score based on the weights
-    # of the words of which they are comprised.
-    lengths = []
-    self.scores = {}
-    for s in self.sentences:
-        sentence = clean(s)
-        # Keep track of each sentence length so we can figure out an
-        # average length later.
-        lengths.append(len(sentence.split()))
-        s_sum = 0.0 # Sum of the weights of the words in this sentence.
-        s_words = 0 # Number of words in the sentence.
-        for s_word in sentence.split():
-            # This works because the Counter object returns 0 if a key
-            # doesn't exist. Stop-words removed earlier will have a value
-            # of 0.
-            s_sum += self.word_weights[s_word]
-            s_words += 1
-
-        # Sentence scores are normalized by length; if it's a word-less
-        # 'sentence' (because of our regex-based splitting algorithm) give
-        # it a value of 0.
-        self.scores[s] = s_sum/s_words if s_words else 0
-
-    self.av_length = sum(lengths)/len(lengths)
-
-    # Despite earlier normalization, sentence scores do depend on length.
-    for sentence in self.sentences:
-        length = len(clean(sentence).split())
-        if length < self.av_length/1.5: # punish short self.sentences
-            try:
-                self.scores[sentence] = float(self.scores[sentence])/3
-            except: pass
-        elif length > self.av_length*1.2: # reward longer self.sentences
-            try:
-                self.scores[sentence] = self.scores[sentence]*3
-            except: pass
-
-    self.sorted_sentences = sorted(
-            self.scores.keys(),
-            key=lambda k: self.scores[k],
-            reverse=True)
-    self.highest_s_score = self.scores[self.sorted_sentences[0]]
-
-    self.sorted_words = sorted(
-            self.word_weights.keys(),
-            key=lambda key: self.word_weights[key],
-            reverse=True)
-    self.highest_w_score = self.word_weights[self.sorted_words[0]]
